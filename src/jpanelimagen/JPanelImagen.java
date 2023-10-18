6bf1bc0de02d7d888baf78a64fc8d1d73d10d97b
danigm11 <danigm821@gmail.com>
2023-10-18 17:04:21 +0200

    segundo video

--- src/jpanelimagen/JPanelImagen.java
+++ src/jpanelimagen/JPanelImagen.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package jpanelimagen;
+import java.awt.Graphics;
+import java.io.File;
 import java.io.Serializable;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 /**
@@ -12,16 +15,25 @@ import javax.swing.JPanel;
  */
 public class JPanelImagen extends JPanel implements Serializable{
     
-    private String rutaImagen;
-    public JPanelImagen(){
-        
-    }
-    public String getRutaImagen() {
+    private File rutaImagen;
+
+    public File getRutaImagen() {
         return rutaImagen;
     }
 
-    public void setRutaImagen(String rutaImagen) {
+    public void setRutaImagen(File rutaImagen) {
         this.rutaImagen = rutaImagen;
     }
+    public JPanelImagen(){
+        
+    }
+   @Override
+   protected void paintComponent(Graphics g){
+       super.paintComponent(g);
+       if(rutaImagen!=null && rutaImagen.exists()){
+       ImageIcon imageIcon= new ImageIcon(rutaImagen.getAbsolutePath());
+       g.drawImage(imageIcon.getImage(),0, 0, this);
+       }
+   }
 
 }
