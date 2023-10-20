9d9d32b755903da52e04450852038994cd0d5b7a
danigm11 <danigm821@gmail.com>
2023-10-20 17:55:57 +0200

    Tercer video

--- src/jpanelimagen/JPanelImagen.java
+++ src/jpanelimagen/JPanelImagen.java
@@ -14,15 +14,16 @@ import javax.swing.JPanel;
  * @author Dani
  */
 public class JPanelImagen extends JPanel implements Serializable{
-    
-    private File rutaImagen;
 
-    public File getRutaImagen() {
-        return rutaImagen;
+    
+    private ImagenFondo imagenFondo;
+
+     public ImagenFondo getImagenFondo() {
+        return imagenFondo;
     }
 
-    public void setRutaImagen(File rutaImagen) {
-        this.rutaImagen = rutaImagen;
+    public void setImagenFondo(ImagenFondo imagenFondo) {
+        this.imagenFondo = imagenFondo;
     }
     public JPanelImagen(){
         
@@ -30,8 +31,8 @@ public class JPanelImagen extends JPanel implements Serializable{
    @Override
    protected void paintComponent(Graphics g){
        super.paintComponent(g);
-       if(rutaImagen!=null && rutaImagen.exists()){
-       ImageIcon imageIcon= new ImageIcon(rutaImagen.getAbsolutePath());
+       if(imagenFondo.getRutaImagen()!=null && imagenFondo.getRutaImagen().exists()){
+       ImageIcon imageIcon= new ImageIcon((imagenFondo.getRutaImagen()).getAbsolutePath());
        g.drawImage(imageIcon.getImage(),0, 0, this);
        }
    }
